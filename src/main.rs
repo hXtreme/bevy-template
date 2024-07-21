a6ee1c75d7b05814a4216d867ddd0c001a446164
Harsh Parekh <harsh_parekh@outlook.com>
2024-07-21 00:48:12 -0400

    Run an empty bevy App on launch

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
-fn main() {
+use bevy::prelude::*;
+
+fn hello_world() {
     println!("Hello, world!");
+}
+
+fn main() {
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_systems(Startup, hello_world)
+        .run();
 }
